310ea5f8bd7218f0f4fbc84cae976814b9da8d32
cadienlake <115520010+cadienlake@users.noreply.github.com>
2023-03-12 17:56:44 -0500

    fixed typo in thought.js model

--- models/Thought.js
+++ models/Thought.js
@@ -29,7 +29,7 @@ const thoughtSchema = new Schema(
 );
 
 // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
-userSchema.virtual("reactionCount").get(function () {
+thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
 
